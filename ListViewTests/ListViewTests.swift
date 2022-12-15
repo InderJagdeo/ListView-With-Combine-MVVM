b34c0ee0c9e14a13b59b1657a40a7c9bc41740dc
Inderjit <inderjit.singh@sdnatech.com>
2022-12-15 12:14:00 +0530

    Working on unit test cases

--- ListViewTests/ListViewTests.swift
+++ ListViewTests/ListViewTests.swift
@@ -6,19 +6,34 @@
 //
 
 import XCTest
-import Foundation
+import Combine
 @testable import ListView
 
 class ListViewTests: XCTestCase {
 
-    override func setUpWithError() throws {
+    var sut: UserListViewModel!
+    
+
+    override func setUp() {
+        sut = UserListViewModel()
+        super.setUp()
     }
 
     override func tearDownWithError() throws {
+        sut = nil
+        try super.tearDownWithError()
     }
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
     }
 
+}
+
+class NetworkManagerMock: NetworkManagerProtocol {
+    var session: URLSession
+
+    func request<Route>(_ route: Route) -> Future<Route.Response, Error> where Route : Router {
+        <#code#>
+    }
 }
